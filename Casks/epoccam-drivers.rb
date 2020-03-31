e6dffad0950a6700a7f539af4a98f2e1bcb06e2a
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-03-31 13:18:24 +0200

    Fix offences

--- Casks/epoccam-drivers.rb
+++ Casks/epoccam-drivers.rb
@@ -2,7 +2,7 @@ cask 'epoccam-drivers' do
   version '1_7'
   sha256 'd1a0681a026917476e87f946653ed1107702f6416f4fa26b40c6a014ee7b902a'
 
-  url "http://kinoconsole.kinoni.com/EpocCam%20Installer_#{version}.pkg.zip"
+  url 'http://kinoconsole.kinoni.com/EpocCam%20Installer_#{version}.pkg.zip'
   name 'EpocCam Drivers'
   homepage 'http://www.kinoni.com/'
 
@@ -12,6 +12,6 @@ cask 'epoccam-drivers' do
 
   uninstall script: {
                       executable: '/Library/CoreMediaIO/Plug-Ins/DAL/EpocCamPlugIn.plugin/Contents/Resources/uninst.sh',
-                      sudo:       true
+                      sudo:       true,
                     }
 end
