eb0501f24b05038747787c3f70c3f572102b6d65
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-08-02 14:25:25 +0200

    Add daily-wall-2 cask

--- Casks/daily-wall.rb
+++ Casks/daily-wall.rb
@@ -1,17 +1,21 @@
 cask 'daily-wall' do
-  version '1.1.2'
-  sha256 '6772023d0990db97ff35d7aa970e6f9879f1bc4a9500bb12c96e57f0e11a8004'
+  version '2.0.1'
+  sha256 'a017bd419608f1c072767252e261424de2463876162d196a5ef6453092fb3582'
 
-  # storage.googleapis.com/updates.dailywall.space/ was verified as official when first introduced to the cask
-  url "https://storage.googleapis.com/updates.dailywall.space/DailyWall-#{version}.dmg"
+  # dailywall-updates.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
+  url "https://dailywall-updates.s3-us-west-2.amazonaws.com/DailyWall-#{version}.dmg"
   name 'DailyWall'
-  homepage 'https://dailywall.space/'
+  homepage 'https://dailywallapp.com/'
 
   app 'DailyWall.app'
 
   zap trash: [
                '~/Library/Application Support/DailyWall',
+               '~/Library/Application Support/com.dailywallapp.app',
                '~/Library/ApplicationSupport/DailyWall',
+               '~/Library/Caches/com.dailywallapp.app',
                '~/Library/Logs/DailyWall',
+               '~/Library/Preferences/com.dailywallapp.app.plist',
+               '~/Library/Preferences/im.rdev.dailywall.*',
              ]
 end
