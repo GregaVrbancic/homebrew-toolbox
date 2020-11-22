eaabbaea911a30481902dbb39632666c5d01fd00
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-11-22 13:25:32 +0100

    fix go wol versioning

--- Formula/go-wol.rb
+++ Formula/go-wol.rb
@@ -3,12 +3,11 @@ class GoWol < Formula
   homepage "https://github.com/sabhiram/go-wol"
 
   version "2.0.0"
-
   if OS.mac?
-    url "https://github.com/sabhiram/go-wol/releases/download/#{version}/wol_darwin_amd64"
+    url "https://github.com/sabhiram/go-wol/releases/download/v#{version}/wol_darwin_amd64"
     sha256 "1dca674c9073b577fbb3616efb30eeab8c4b7e18c12bf759aed6e9852b29186c"
   else
-    url "https://github.com/sabhiram/go-wol/releases/download/#{version}/wol_linux_amd64"
+    url "https://github.com/sabhiram/go-wol/releases/download/v#{version}/wol_linux_amd64"
     sha256 "d2ff14f43850922a96f8f5f8af456917fe7d0ba42804604bb439def30e01b55e"
   end
 
