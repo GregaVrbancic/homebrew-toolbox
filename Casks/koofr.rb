a30df0b29cbc16dacba1bf94e2262f5ab3f1156f
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-12-30 13:09:31 +0100

    Add koofr cask

---
+++ Casks/koofr.rb
@@ -0,0 +1,11 @@
+cask 'koofr' do
+    sha256 '821f50af2b27f8a3487337ecc5fbad90d6f7329e9eaff7feb2f577957597d016'
+
+    # app.koofr.net was verified as official when first introduced to the cask
+    url "https://app.koofr.net/dl/apps/osx"
+    name 'Koofr'
+    homepage 'https://koofr.er/'
+
+    app 'Koofr.app'
+
+  end
