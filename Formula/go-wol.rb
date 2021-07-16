9cf142bec3c1da962ecc927c0eda847d7c67bf0a
Grega Vrbančič <grega.vrbancic@gmail.com>
2021-07-16 08:21:40 +0200

    Update go-wol to 2.0.1 (#13)

--- Formula/go-wol.rb
+++ Formula/go-wol.rb
@@ -2,13 +2,13 @@ class GoWol < Formula
   desc "Simple wake on LAN magic packet generator for golang"
   homepage "https://github.com/sabhiram/go-wol"
 
-  version "2.0.0"
+  version "2.0.1"
   if OS.mac?
     url "https://github.com/sabhiram/go-wol/releases/download/v#{version}/wol_darwin_amd64"
-    sha256 "1dca674c9073b577fbb3616efb30eeab8c4b7e18c12bf759aed6e9852b29186c"
+    sha256 "9c292e17d4a9e3830a4ef1cc5b351ff15096d166d2bb74e5a35001130146d9a0"
   else
     url "https://github.com/sabhiram/go-wol/releases/download/v#{version}/wol_linux_amd64"
-    sha256 "d2ff14f43850922a96f8f5f8af456917fe7d0ba42804604bb439def30e01b55e"
+    sha256 "be82d36dc257d3643abfaa9077c5b3ffbb2dfb8c987054a020969a0ee96676ab"
   end
 
   def install
