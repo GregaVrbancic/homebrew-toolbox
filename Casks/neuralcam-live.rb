4b9763be4407420b6b86949b0c560c7e7c7c4f27
Grega Vrbančič <grega.vrbancic@gmail.com>
2021-11-21 14:45:20 +0100

    Update neuralcam-live to 1.1.1

--- Casks/neuralcam-live.rb
+++ Casks/neuralcam-live.rb
@@ -1,6 +1,6 @@
 cask 'neuralcam-live' do
-  version '1.0.0'
-  sha256 "88526d7c76683a4b4557d0e46607463153447984f2e1421e7146230f92057484"
+  version '1.1.1'
+  sha256 "6eaa27868c9c92765afab885e65a65fa07708635aa01b760525917dcae38a36a"
 
   url "https://neural.cam/live/NeuralCamLive-installer-#{version}.pkg"
   name "NeuralCam Live"
