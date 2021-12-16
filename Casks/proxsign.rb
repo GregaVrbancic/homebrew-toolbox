5d4be379492f2e5dee807c629e31915dcb7eb9eb
Grega Vrbančič <grega.vrbancic@gmail.com>
2021-12-16 09:58:27 +0100

    Fix proxsign cask definition

--- Casks/proxsign.rb
+++ Casks/proxsign.rb
@@ -1,11 +1,12 @@
 cask 'proxsign' do
-  sha256 '085b7d017c2145b32116b26f296576bb61d0492ee8060cf465418348090663de'
+  version :latest
+  sha256 :no_check
 
   url "https://public.setcce.si/proxsign/update/mac/SETCCE_proXSign.pkg"
   name 'proXSign'
   homepage 'https://proxsign.setcce.si/proXSignCustomerPages/index2.html'
 
-  depends_on macos: '>= :mojave'
+  depends_on macos: '>= :catalina'
 
   pkg 'SETCCE_proXSign.pkg'
 
